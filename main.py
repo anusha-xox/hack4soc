018d4f20179dc36e994b3e9ab00c78ac7b421fcc
Akanksha <admin@gmail.com>
2022-08-27 13:46:25 +0530

    3rd commit!!

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from flask_ckeditor import CKEditor, CKEditorField
 import datetime
 import bleach
 import smtplib
+from login import LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -15,9 +16,23 @@ app.config['CKEDITOR_PKG_TYPE'] = 'full-all'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
-@app.route('/')
-def get_all_posts():
+
+@app.route('/', methods=["GET", "POST"])
+def login():
+    login_form = LoginForm()
+
+    if login_form.validate_on_submit():
+        if login_form.username.data == 'admin' and login_form.password.data == 'ab':
+
+            return render_template('index.html')
+
+    return render_template('login.html', form=login_form)
+
+
+@app.route('/home')
+def home():
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
